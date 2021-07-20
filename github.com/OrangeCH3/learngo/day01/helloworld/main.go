913632164c4250a4deae3d7c460104d44178fb14
OrangeCH3 <hanjie1225@126.com>
2021-07-20 22:34:15 +0800

    Second commit

--- github.com/OrangeCH3/learngo/day01/helloworld/main.go
+++ github.com/OrangeCH3/learngo/day01/helloworld/main.go
@@ -1,14 +1,14 @@
 /*
-@Time : 2021/7/20 22:11
+@Time : 2021/7/20 22:32
 @Author : OrangeCH3
-@File : main
 @Software: GoLand
+@File : main
 */
 
 package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	fmt.Println("OrangeCH3, Let's go!")
 }
