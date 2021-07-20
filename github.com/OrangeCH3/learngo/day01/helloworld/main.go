90a7db121dbfa0626615469ad4c10f403f09684a
OrangeCH3 <hanjie1225@126.com>
2021-07-20 22:40:30 +0800

    Cont2nd-3rd commit

--- github.com/OrangeCH3/learngo/day01/helloworld/main.go
+++ github.com/OrangeCH3/learngo/day01/helloworld/main.go
@@ -5,10 +5,17 @@
 @File : main
 */
 
+// go build -o xxx.exe 指定名称构建可执行文件
+
 package main
 
+// 导入语句
 import "fmt"
 
+// 函数外部只能放置标识符（变量/常量/函数/类型）的声明
+// fmt.Println("OrangeCH3, Let's go!") 非法
+
+// 程序的入口函数
 func main() {
 	fmt.Println("OrangeCH3, Let's go!")
 }
