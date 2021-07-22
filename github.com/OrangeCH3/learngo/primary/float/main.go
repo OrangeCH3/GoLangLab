73be7b9b9d21c0c03868acf05223af5327fe8521
OrangeCH3 <hanjie1225@126.com>
2021-07-22 09:44:43 +0800

    Eleventh commit

---
+++ github.com/OrangeCH3/learngo/primary/float/main.go
@@ -0,0 +1,18 @@
+/*
+@Time : 2021/7/21 20:32
+@Author : OrangeCH3
+@Software: GoLand
+@File : main
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// fmt.Println(math.MaxFloat32)
+
+	f1 := 1.23456
+	fmt.Printf("%T\n", f1)
+}
