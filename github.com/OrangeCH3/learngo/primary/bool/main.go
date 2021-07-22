ddf5a80cf33494f69e4aa001ed9887d20b105024
OrangeCH3 <hanjie1225@126.com>
2021-07-22 09:46:02 +0800

    Twelfth commit

---
+++ github.com/OrangeCH3/learngo/primary/bool/main.go
@@ -0,0 +1,21 @@
+/*
+@Time : 2021/7/21 20:42
+@Author : OrangeCH3
+@Software: GoLand
+@File : main
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	b1 := true
+
+	// 默认声明
+	var b2 bool
+
+	fmt.Printf("Type: %T Value: %v\n", b1, b1)
+	fmt.Printf("Type: %T Value: %v\n", b2, b2)
+}
