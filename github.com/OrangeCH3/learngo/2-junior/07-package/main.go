44621804fc2a4d7a7455e20aeb64a58e627912c0
OrangeCH3 <hanjie1225@126.com>
2021-08-17 10:32:50 +0800

    Sixtieth commit

---
+++ github.com/OrangeCH3/learngo/2-junior/07-package/main.go
@@ -0,0 +1,19 @@
+/*
+@Time : 2021/8/17 10:27
+@Author : OrangeCH3
+@Software: GoLand
+@File : main
+*/
+
+package main
+
+import (
+	"fmt"
+	m "github.com/OrangeCH3/learngo/2-junior/07-package/pkg_test"
+)
+
+func main() {
+
+	fmt.Println(m.Add(100, 200))
+	fmt.Println(m.Mode)
+}
