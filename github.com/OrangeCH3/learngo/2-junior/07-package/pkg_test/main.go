5ef612697df272a1aef8dc2d1688f5d8bb5222e8
OrangeCH3 <hanjie1225@126.com>
2021-08-17 10:32:37 +0800

    Fifty-ninth commit

---
+++ github.com/OrangeCH3/learngo/2-junior/07-package/pkg_test/main.go
@@ -0,0 +1,35 @@
+/*
+@Time : 2021/8/17 10:29
+@Author : OrangeCH3
+@Software: GoLand
+@File : main
+*/
+
+package pkg2
+
+import "fmt"
+
+func init() {
+	fmt.Println("This is pkg_test init() function!")
+}
+
+// 包变量可见性
+
+var a = 100 // 首字母小写，外部包不可见，只能在当前包内使用
+
+// Mode 首字母大写外部包可见，可在其他包中使用
+const Mode = 36
+
+type person struct { // 首字母小写，外部包不可见，只能在当前包内使用
+	name string
+}
+
+// Add 首字母大写，外部包可见，可在其他包中使用
+func Add(x, y int) int {
+	return x + y
+}
+
+func age() { // 首字母小写，外部包不可见，只能在当前包内使用
+	var Age = 18 // 函数局部变量，外部包不可见，只能在当前函数内使用
+	fmt.Println(Age)
+}
